64b1376e6abfd7fdd212d5e505b11213aa7ff5e6
Mardoqueo <jeffreymardoqueo260@gmail.com>
2025-04-04 11:38:04 -0600

    No se que uhice

--- AdventumCaez-API-BL/obj/Debug/net8.0/AdventumCaez-API-BL.AssemblyInfo.cs
+++ AdventumCaez-API-BL/obj/Debug/net8.0/AdventumCaez-API-BL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventumCaez-API-BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdd4d486304ae0b2e41466b61a6dabc886ef17df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+793051103a84683c6abc15eb3376893e16faf263")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventumCaez-API-BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventumCaez-API-BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
